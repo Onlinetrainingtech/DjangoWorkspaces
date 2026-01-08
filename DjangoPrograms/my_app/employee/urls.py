5dd2a887cb004a580d695c51b1d141da95b8fed6
azartechnet <azartechnet@gmail.com>
2026-01-08 07:49:03 +0530

    updated

--- DjangoPrograms/my_app/employee/urls.py
+++ DjangoPrograms/my_app/employee/urls.py
@@ -5,4 +5,5 @@ urlpatterns=[
     path('about/',views.about,name='about'),
     path('contact/',views.contact,name='contact'),
     path('login/',views.login_view,name='login'),
+    path('register/',views.register_view,name='register'),
 ]
--- DjangoPrograms/my_app/employee/views.py
+++ DjangoPrograms/my_app/employee/views.py
@@ -2,11 +2,14 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse("welcome to the Application")
+    # return HttpResponse("welcome to the Application")
+    return render(request, 'home.html')
 def about(request):
-    return HttpResponse("This is the about page of the Application")
+    # return HttpResponse("This is the about page of the Application")
+    return render(request, 'about.html')
 def contact(request):
-    return HttpResponse("This is the contact page of the Application")
+    # return HttpResponse("This is the contact page of the Application")
+    return render(request, 'contact.html')
 def login_view(request):
     if request.method == 'POST':
         username = request.POST.get('username')
@@ -17,4 +20,6 @@ def login_view(request):
         else:
             return render(request, 'login.html', {'error': 'Invalid credentials'})
     return render(request, 'login.html')
+def register_view(request):
+    return render(request, 'register.html')
 
