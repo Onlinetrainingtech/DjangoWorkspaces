dfd296a938a54184db1afa52dc778d29a4b9c675
azartechnet <azartechnet@gmail.com>
2026-01-09 13:00:42 +0530

    updated

--- DjangoPrograms/my_app/employee/models.py
+++ DjangoPrograms/my_app/employee/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+class Employee(models.Model):
+    name=models.CharField(max_length=100)
+    email=models.EmailField()
+    salary=models.IntegerField()
 
-# Create your models here.
+    def __str__(self):
+        return self.name
+    
--- DjangoPrograms/my_app/employee/views.py
+++ DjangoPrograms/my_app/employee/views.py
@@ -1,15 +1,24 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from django.shortcuts import redirect
+from .forms import EmployeeForm
 def home(request):
     # return HttpResponse("welcome to the Application")
     return render(request, 'home.html')
 def about(request):
     # return HttpResponse("This is the about page of the Application")
     return render(request, 'about.html')
+
 def contact(request):
-    # return HttpResponse("This is the contact page of the Application")
-    return render(request, 'contact.html')
+    if request.method=='POST':
+        form=EmployeeForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('contact')
+    else:
+        form=EmployeeForm()
+    return render(request, 'contact.html', {'form': form})
+
 def login_view(request):
     if request.method == 'POST':
         username = request.POST.get('username')
